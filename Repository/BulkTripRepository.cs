f189c803d4ec2736151b29ab0a70ad148deda5e6
Matthew Novelli <mattnovelli@gmail.com>
2025-05-12 13:03:29 -0500

    add TruncateTripToCalendarDateBridgeTable

--- Repository/BulkTripRepository.cs
+++ Repository/BulkTripRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 using Mtd.Infrastructure.EFCore.Bulk.Repository;
 using Mtd.Stopwatch.Core.Entities.Transit;
 using Mtd.Stopwatch.Core.Repositories.Bulk.Transit;
@@ -11,4 +13,6 @@ public class BulkTripRepository(ITripRepository<IReadOnlyCollection<Trip>> tripR
 	private readonly ITripRepository<IReadOnlyCollection<Trip>> _tripRepository = tripRepository ?? throw new ArgumentNullException(nameof(tripRepository));
 
 	public Task<IReadOnlyCollection<Trip>> GetAllWithRoutesAsync(CancellationToken cancellationToken) => _tripRepository.GetAllWithRoutesAsync(cancellationToken);
+	public Task TruncateTripToCalendarDateBridgeTable(CancellationToken cancellationToken) => _dbContext.Database.ExecuteSqlRawAsync("TRUNCATE TABLE transit.TripToCalendarDate;"
+, cancellationToken: cancellationToken);
 }
